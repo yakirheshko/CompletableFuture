613dce0cd6b13eead98764de898f71d329ddc4d3
u726889 <yakir.hershkoviz@finastra.com>
2022-05-03 09:45:34 +0300

    send our own executor along with runnable in completableFuture

--- src/main/java/AsyncExample.java
+++ src/main/java/AsyncExample.java
@@ -1,16 +1,19 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class AsyncExample {
 
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
         Runnable runnable = () -> {
           delay(1);
             System.out.println("I am in Runnable - " + Thread.currentThread().getName());
 
         };
 
-        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(runnable);
+        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(runnable, executorService);
 
         System.out.println("I am in main - " + Thread.currentThread().getName());
 
